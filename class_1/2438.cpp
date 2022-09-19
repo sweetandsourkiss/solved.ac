d074f53a23cc4bb28d11a50fe395913f6426076a
Junho Son <106135040+sweetandsourkiss@users.noreply.github.com>
2022-09-19 21:46:51 +0900

    Add files via upload

---
+++ class_1/2438.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int N;
+	cin >> N;
+
+	for (int i = 1; i < N+1; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
